cf08bccc0ae300601bb705e2840885a65b180c37
Erico Almeida <ericoalmeidamvp@gmail.com>
2020-09-12 18:37:09 -0400

    Added extension to create component

--- src/commands/page.ts
+++ src/commands/page.ts
@@ -7,34 +7,11 @@ module.exports = {
     run: async (toolbox: GluegunToolbox) => {
         const {
             parameters,
-            filesystem: { read },
-            template: {generate},
-            print: {success, error}
+            createComponent
         } = toolbox;
-
-       if (!parameters.first){
-           error('Page name must be specified.');
-           return;
-       }
 
        const name = parameters.first;
 
-       const packageList = await read('package.json', 'json');
-       const isReactNative = !!packageList.dependencies['react-native'];
-
-       const template = isReactNative ? 'styles-rn.js.ejs' : 'styles-react.js.ejs';
-
-       await generate({
-           template: 'page.js.ejs',
-           target: `src/pages/${name}/index.tsx`,
-           props: { name }
-       })
-
-       await generate({
-           template,
-           target: `src/pages/${name}/styles.ts`,
-        })
-
-        success(`Generated ${name} page.`) 
-    } 
+       await createComponent('src/pages', name);
+    }
 }
---
+++ src/extensions/create-component.ts
@@ -0,0 +1,40 @@
+import { GluegunToolbox } from 'gluegun';
+
+module.exports = (toolbox: GluegunToolbox) => {
+    const { filesystem: {read}, print: {success, error}, template: {generate} } = toolbox;
+
+    async function isReactNative() {
+        const packageList = await read('package.json', 'json');
+
+        return !!packageList.dependencies['react-native'];
+    }
+
+    async function createComponent(folder: String, name: String) {
+        if (!folder){
+            error('Folder must be specified.');
+            return;
+        }
+
+        if (!name){
+            error('Name must be specified.');
+            return;
+        }
+
+        await generate({
+            template: 'component.js.ejs',
+            target: `${folder}/${name}/index.tsx`,
+            props: { name }
+        })
+
+        const template = (await isReactNative()) ? 'styles-rn.js.ejs' : 'styles-react.js.ejs';
+
+        await generate({
+            template,
+            target: `${folder}/${name}/styles.ts`,
+         })
+
+         success(`Generated ${folder}/${name}.`)
+    }
+
+    toolbox.createComponent = createComponent;
+}
